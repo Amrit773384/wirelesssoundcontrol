727872ff89a90755f0aa3a53379ad696422d19a3
Amrit Singh <amrit773384@gmail.com>
2023-03-27 20:35:45 +0530

    have 3 phases of project, pending - to connect them

---
+++ main.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter.ttk import Radiobutton
+from tkinter import PhotoImage
+from threading import Thread
+import VolumeController
+# import gesturelib
+
+class GUI:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Wireless Sound Control")
+        self.root.geometry("1000x600")
+        self.root.minsize(300,200)
+
+        self.upperframe = tk.Frame(self.root,background='white',height=20)
+        self.upperframe.pack(side='top',fill='x',padx=0,anchor='n',pady=10)
+
+        self.label = tk.Label(self.upperframe,text='Wireless Sound Control',font='times 18 bold',background='white')
+        self.label.pack(side='top',pady=10)
+
+        self.canv = tk.Canvas(self.upperframe)
+        self.canv.pack(side='top')
+
+        self.button_1 = Radiobutton(self.canv,text='Typing')
+        self.button_1.pack(side='left',pady=5,padx=10)
+
+        self.button_2 = Radiobutton(self.canv,text='Notepad')
+        self.button_2.pack(side='right',pady=5,padx=10)
+
+        self.bottomframe = tk.Frame(self.root,background='lightgrey')
+        self.bottomframe.pack(expand=True,fill='both',side='top',anchor='n')
+
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    tk_object = tk.Tk()
+    GUI(root = tk_object)
