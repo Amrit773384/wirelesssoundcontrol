289db509ddba1ced55f76f54e7f361dbdddd5e21
Amrit Singh <amrit773384@gmail.com>
2023-04-13 21:17:35 +0530

    gui done

--- main.py
+++ main.py
@@ -7,66 +7,93 @@ from tkinter.ttk import Radiobutton
 from tkinter import PhotoImage
 from PIL import ImageTk, Image
 #gesture resources 
-from function import *
-from keras.utils import to_categorical
-from keras.models import model_from_json
-from keras.layers import LSTM, Dense
-from keras.callbacks import TensorBoard
+# from function import *
+# from keras.utils import to_categorical
+# from keras.models import model_from_json
+# from keras.layers import LSTM, Dense
+# from keras.callbacks import TensorBoard
 #volume control libraries
 import cv2 
-import mediapipe as mp
-from math import hypot
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import numpy as np 
+# import mediapipe as mp
+# from math import hypot
+# from ctypes import cast, POINTER
+# from comtypes import CLSCTX_ALL
+# from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+# import numpy as np 
 
 
 class GUI:
     def __init__(self,root):
         self.root = root
         self.root.title("Wireless Sound Control")
-        self.root.geometry("1000x600")
-        self.root.minsize(300,200)
+        self.root.geometry("600x700")
+        self.root.resizable(False,False)
 
-        self.upperframe = tk.Frame(self.root,background='white',height=20)
-        self.upperframe.pack(side='top',fill='x',padx=0,anchor='n',pady=10)
-
-        self.label = tk.Label(self.upperframe,text='Wireless Sound Control',font='times 18 bold',background='white')
-        self.label.pack(side='top',pady=10)
-
-        self.canv = tk.Canvas(self.upperframe)
-        self.canv.pack(side='top')
-
-        self.button_1 = Radiobutton(self.canv,text='Typing')
-        self.button_1.pack(side='left',pady=5,padx=10)
-
-        self.button_2 = Radiobutton(self.canv,text='Notepad')
-        self.button_2.pack(side='right',pady=5,padx=10)
-
-        self.bottomframe = tk.Frame(self.root,background='lightgrey')
-        self.bottomframe.pack(expand=True,fill='both',side='top',anchor='n')
-
-        self.notepad = tk.Text(self.bottomframe,)
-        self.notepad.pack(fill='both',side='left')
-
-        #Capture video frames
-        cap = cv2.VideoCapture(1)
-        _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        img = Image.fromarray(cv2image)
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.camera = tk.Label(self.bottomframe,width=400,height=500,image=imgtk)
+        self.upperframe = tk.Frame(self.root,background='white',height=400,width=600)
+        self.upperframe.pack(side='top',fill='x',anchor='n',padx=10,pady=10)
+        self.cap = cv2.VideoCapture(1)
+        img = self.cap.read()[1]
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = ImageTk.PhotoImage(Image.fromarray(img).resize((self.upperframe['width'],self.upperframe['height']),Image.Resampling.LANCZOS)) 
+        self.camera = tk.Label(self.upperframe,image=img)
         self.camera.pack(side='right')
 
-        self.gesture_Thread = Thread(target=self.get_alpha_from_gesture)
-        self.gesture_Thread.setDaemon(True)
-        self.gesture_Thread.start()
+
+        self.notepad_frame = tk.Frame(self.root,width=600,height=200) 
+        self.notepad_frame.pack(expand=False,side='top')
+
+        self.notepad = tk.Text(self.notepad_frame,font='consolas 14',height=7)
+        self.notepad.pack(side='top',padx=10)
+
+
+        self.bottomframe = tk.Frame(self.root,height=100,width = 600)
+        self.bottomframe.pack(fill='both',side='top',padx=10,pady=10)
+
+        commonfont = 'arial 16 '
+
+        self.space_button = tk.Button(self.bottomframe,
+                                    text="Space",
+                                    font=commonfont,
+                                    background='AntiqueWhite1',
+                                    padx=20,pady=10,
+                                    command=lambda:self.notepad.insert('end', ' ')
+                                    )
+        self.space_button.grid(row=0,column=0,padx=55,pady=10)
         
-        self.root.mainloop()
+        self.clear_button = tk.Button(self.bottomframe,
+                                    text='Clear',
+                                    font=commonfont,
+                                    background='lightgreen',
+                                    padx=20,pady=10,
+                                    command=lambda:self.notepad.delete(1.0,'end')
+                                    )
+        self.clear_button.grid(row=0,column=1,pady=10)
 
+        self.savetext_button = tk.Button(self.bottomframe,
+                                    text='Save',
+                                    font=commonfont,
+                                    background='skyblue',
+                                    padx=20,pady=10,
+                                    command=self.save
+                                    )
+        self.savetext_button.grid(row=0,column=2,padx=55,pady=10)
 
+        # self.gesture_Thread = Thread(target=self.get_alpha_from_gesture)
+        # self.gesture_Thread.setDaemon(True)
+        # self.gesture_Thread.start()
+        self.notepad.focus_set()
+        # while True :
+        #     try:
+        #         img = self.cap.read()[1]
+        #         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        #         img = ImageTk.PhotoImage(Image.fromarray(img).resize((self.upperframe['width'],self.upperframe['height']),Image.Resampling.LANCZOS)) 
+        #         self.camera['image'] = img
+        #         self.root.update()
+        #     except:break
+    def save(self):
+        print(self.notepad.get(1.0, 'end'))
+        print("text save ..")
+        
     def get_alpha_from_gesture(self):
         json_file = open("model.json", "r")
         model_json = json_file.read()
@@ -160,11 +187,11 @@ class GUI:
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 
                 # Show to screen
-                b,g,r = cv2.split(frame)
-                img = cv2.merge((r,g,b))
-                im = Image.fromarray(img)
-                imgtk = ImageTk.PhotoImage(image=im) 
-                self.camera.configure(image=imgtk)
+                img = cap.read()[1]
+                img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+                img = ImageTk.PhotoImage(Image.fromarray(img)) 
+                self.camera.configure(image=img)
+                self.root.update()
                 # Break gracefully
 
 if __name__ == '__main__':
